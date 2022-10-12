2dc80adf53df1f2957513316c038f95c263e183b
Chandra Kishore Danduri <93044238+perfmonweb@users.noreply.github.com>
2022-10-12 00:09:24 -0700

    web: minor style changes

--- web/src/utils/themes/theme.ts
+++ web/src/utils/themes/theme.ts
@@ -6,13 +6,13 @@ export interface ThemeProps {
 }
 
 export const lightTheme: ThemeProps = {
-  body: 'linear-gradient(#6700f8, #fff)',
+  body: 'linear-gradient(#0055ff, #ffffff)',
   text: '#363537',
   toggleBorder: '#b8b7b7',
   background: '#1e1e1e',
 };
 export const darkTheme: ThemeProps = {
-  body: 'linear-gradient(#e9133ad1, #000)',
+  body: 'linear-gradient(#1e13e9, #000)',
   text: '#FAFAFA',
   toggleBorder: '#6B8096',
   background: '#1e1e1e',
