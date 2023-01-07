19253075805d3ec64189df06f2beb557f621a5d6
Chandra Kishore Danduri <93044238+perfmonweb@users.noreply.github.com>
2023-01-06 22:28:41 -0800

    Jan 6, 2023

--- server/src/postgresql/subscriptions/user.subscription.ts
+++
@@ -1,6 +0,0 @@
-import { EntitySubscriberInterface, EventSubscriber } from 'typeorm';
-
-import { User } from '../entities/User';
-
-@EventSubscriber()
-export class userSubscriber implements EntitySubscriberInterface<User> {}
