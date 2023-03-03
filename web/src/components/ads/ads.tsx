f4bc99c62ab8f3cb8bf88ede9c92ee37b5d516f0
Chandra Kishore Danduri <chandra.kishore180994@gmail.com>
2023-03-02 23:40:40 -0800

    Removed Google Adsense

--- web/src/components/ads/ads.tsx
+++ web/src/components/ads/ads.tsx
@@ -2,29 +2,7 @@ import { useEffect } from 'react';
 
 type props = {};
 const Ads: React.FC<props> = ({}) => {
-  useEffect(() => {
-    const installGoogleAds = () => {
-      const elem = document.createElement('script');
-      elem.src =
-        'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-      elem.async = true;
-      elem.defer = true;
-      document.body.insertBefore(elem, document.body.firstChild);
-    };
-    installGoogleAds();
-    ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
-  }, []);
-  return (
-    <div id='adsense-ad'>
-      <ins
-        className='adsbygoogle'
-        style={{ display: 'block' }}
-        data-ad-client='ca-pub-5406977689017300'
-        data-ad-slot='12121212'
-        data-ad-format='auto'
-      />
-    </div>
-  );
+  return <div id='adsense-ad'></div>;
 };
 
 export default Ads;
