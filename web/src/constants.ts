e900f8c8eb2131329f85a753af5d153acccbe3e8
Chandra Kishore Danduri <chandra.kishore180994@gmail.com>
2023-03-26 12:40:29 -0700

    Added affiliation notice.

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,7 +1,7 @@
 export const home = {
   heading: 'Now stream movies with comments',
   sub: 'Moovy provides you a new way to enjoy your streaming experience with a comment section and video filters.',
-  sub2: 'Why wait? Install our extension and step up the game with your streaming experience.',
+  sub2: 'Why wait? Install our extension and step up the game with your streaming experience. Please note that We are not affiliated with Netflix.',
   supported: 'Supported Platforms:',
 };
 
