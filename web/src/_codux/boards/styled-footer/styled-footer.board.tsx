019c8396745eccdf160f27a91796ead4c77c8f3c
Chandra Kishore Danduri <chandra.kishore180994@gmail.com>
2023-04-09 17:00:45 -0700

    Added footer styles

--- web/src/_codux/boards/styled-footer/styled-footer.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { StyledFooter } from '../../../pages/welcome/footer/footer.styles';
-
-export default createBoard({
-  name: 'StyledFooter',
-  Board: () => <StyledFooter />,
-});
