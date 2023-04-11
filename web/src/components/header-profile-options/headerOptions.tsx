ce017f82dfe57ef0214ac4494e07535c54c3ef87
Chandra Kishore Danduri <chandra.kishore180994@gmail.com>
2023-04-11 12:17:20 -0700

    MovieCommentsConnection is in progress

--- web/src/components/header-profile-options/headerOptions.tsx
+++ web/src/components/header-profile-options/headerOptions.tsx
@@ -1,18 +1,21 @@
 /// <reference types="chrome"/>
-import { EXT_ID, isServer } from '../../constants';
-import { MdOutlineExitToApp, MdPerson, MdSync } from 'react-icons/md';
+import { EXTENSION_URL, EXT_ID, isServer } from '../../constants';
+import { MdInstallDesktop, MdOutlineExitToApp, MdPerson, MdSync } from 'react-icons/md';
+import { MouseEventHandler, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import {
   useGetUserFullNameQuery,
   useLogoutMutation,
 } from '../../generated/graphql';
 
-import { MouseEventHandler } from 'react';
 import { StyledHeaderOptions } from './headerOptions.styles';
 import { sliceResetUser } from '../../redux/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
 
 const HeaderOptions = () => {
+    const [isExtensionInstalled, setIsExtensionInstalled] =
+      useState<boolean>(false);
+
   const [, logout] = useLogoutMutation();
   const user = useAppSelector(state => state.user);
   const navigate = useNavigate();
@@ -26,6 +29,36 @@ const HeaderOptions = () => {
     e.stopPropagation();
     navigate(`/home/profile/${user.nickname}`);
   };
+
+  useEffect(() => {
+    type CheckExtensionCallback = (installed: boolean) => void;
+    const checkExtension = (callback: CheckExtensionCallback): void => {
+      try{
+ // Send a message to the extension with the specified ID
+      chrome.runtime.sendMessage(
+        EXT_ID,
+        { message: 'checkExtension' },
+        (response: any) => {
+          // If there's a response, the extension is installed and enabled
+          if (response) {
+            callback(true);
+          } else {
+            // No response indicates the extension is not installed or not enabled
+            callback(false);
+          }
+        },
+      );
+      }catch(e){
+         callback(false);
+      }
+     
+    };
+    checkExtension((installed: boolean) => {
+      setIsExtensionInstalled(installed);
+    });
+  }, []);
+
+
   const logOutHandler: MouseEventHandler<HTMLDivElement> = e => {
     e.stopPropagation();
     logout({}).then(res => {
@@ -37,7 +70,6 @@ const HeaderOptions = () => {
       if (data) {
         navigate('/');
         dispatch(sliceResetUser());
-        return;
       }
     });
   };
@@ -63,27 +95,39 @@ const HeaderOptions = () => {
         </div>
         <div className="text">Logout</div>
       </div>
-      <div
-        className="option"
-        tabIndex={0}
-        onClick={e => {
-          e.stopPropagation();
-          console.log({ EXT_ID });
-          chrome.runtime.sendMessage(
-            EXT_ID,
-            { type: 'EXTENSION_LOG_IN', user: user },
-            response => {
-              console.log('response', response);
-            },
-          );
-        }}
-        id="sync-login"
-      >
-        <div className="icon">
-          <MdSync size={20} />
+      {isExtensionInstalled ? (
+        <div
+          className="option"
+          tabIndex={0}
+          onClick={e => {
+            e.stopPropagation();
+            console.log({ EXT_ID });
+            chrome.runtime.sendMessage(
+              EXT_ID,
+              { type: 'EXTENSION_LOG_IN', user: user },
+              response => {
+                console.log('response', response);
+              },
+            );
+          }}
+          id="sync-login"
+        >
+          <div className="icon">
+            <MdSync size={20} />
+          </div>
+          <div className="text">Sync Login with Extension</div>
         </div>
-        <div className="text">Sync Login with Extension</div>
-      </div>
+      ) : (
+        <div className="option" onClick={(e)=>{
+          e.stopPropagation();
+          window.open(EXTENSION_URL, '_blank');
+        }}>
+          <div className="icon">
+            <MdInstallDesktop size={20} />
+          </div>
+          <div className="text">Install Extension</div>
+        </div>
+      )}
     </StyledHeaderOptions>
   );
 };
