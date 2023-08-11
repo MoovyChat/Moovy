6c216e5201da3527dc39a01056a0dab2a5d4c882
Chandra Kishore Danduri <kishore@Chandras-MacBook-Pro.local>
2023-08-11 08:09:02 -0700

    Updating app design

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import viteCompression from 'vite-plugin-compression';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [svgr(), react(), viteCompression()],
   server: {
     port: 3000,
   },
   build: {
+    sourcemap: true,
     outDir: 'build',
   },
 });
